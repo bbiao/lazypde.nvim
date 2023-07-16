120605b3d675e3638c92f5ef35fad707463cdaad
bbiao <41536+bbiao@users.noreply.github.com>
2023-07-16 11:15:15 +0800

    plugins: enable more LazyVim extras; use mini-stater instead of alpha-nvim

--- lua/lazypde/plugins/lazy.lua
+++ lua/lazypde/plugins/lazy.lua
@@ -1,16 +1,25 @@
 return {
-  { import = "lazyvim.plugins.extras.vscode" },
   { import = "lazyvim.plugins.extras.dap.core" },
-  { import = "lazyvim.plugins.extras.test.core" },
-  { import = "lazyvim.plugins.extras.lang.go" },
   { import = "lazyvim.plugins.extras.dap.nlua" },
   { import = "lazyvim.plugins.extras.formatting.prettier" },
+  { import = "lazyvim.plugins.extras.test.core" },
   { import = "lazyvim.plugins.extras.ui.edgy" },
-
+  { import = "lazyvim.plugins.extras.ui.mini-animate" },
   -- use mini.starter instead of alpha
-  -- { import = "lazyvim.plugins.extras.ui.mini-starter" },
+  { import = "lazyvim.plugins.extras.ui.mini-starter" },
+  { import = "lazyvim.plugins.extras.vscode" },
+
+  { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
   { import = "lazyvim.plugins.extras.util.project" },
 
+  { import = "lazyvim.plugins.extras.editor.leap" },
+  { import = "lazyvim.plugins.extras.editor.mini-files" },
+
+  { import = "lazyvim.plugins.extras.lang.docker" },
+  { import = "lazyvim.plugins.extras.lang.go" },
   -- add jsonls and schemastore packages, and setup treesitter for json, json5 and jsonc
   { import = "lazyvim.plugins.extras.lang.json" },
+  { import = "lazyvim.plugins.extras.lang.python" },
+  { import = "lazyvim.plugins.extras.lang.ruby" },
+  { import = "lazyvim.plugins.extras.lang.rust" },
 }
