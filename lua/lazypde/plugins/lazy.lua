fd5706ba3a2a69f79dd89e7241e0b516084596c4
bbiao <41536+bbiao@users.noreply.github.com>
2023-07-24 15:40:56 +0800

    chore(LazyVim): adapter LazyVim v6.1.0

--- lua/lazypde/plugins/lazy.lua
+++ lua/lazypde/plugins/lazy.lua
@@ -12,15 +12,21 @@ return {
   -- { import = "lazyvim.plugins.extras.editor.leap" },
   { import = "lazyvim.plugins.extras.editor.mini-files" },
 
+  { import = "lazyvim.plugins.extras.coding.yanky" },
+
   { import = "lazyvim.plugins.extras.dap.core" },
   { import = "lazyvim.plugins.extras.dap.nlua" },
   { import = "lazyvim.plugins.extras.formatting.prettier" },
+  { import = "lazyvim.plugins.extras.lang.clangd" },
+  { import = "lazyvim.plugins.extras.lang.cmake" },
   { import = "lazyvim.plugins.extras.lang.docker" },
   { import = "lazyvim.plugins.extras.lang.go" },
+  -- { import = "lazyvim.plugins.extras.lang.java" },
   -- add jsonls and schemastore packages, and setup treesitter for json, json5 and jsonc
   { import = "lazyvim.plugins.extras.lang.json" },
   { import = "lazyvim.plugins.extras.lang.python" },
   { import = "lazyvim.plugins.extras.lang.ruby" },
   { import = "lazyvim.plugins.extras.lang.rust" },
+  { import = "lazyvim.plugins.extras.lang.tex" },
   { import = "lazyvim.plugins.extras.test.core" },
 }
