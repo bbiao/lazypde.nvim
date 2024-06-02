c698619c6ba66d08aaa6ff4fa2ddce1c0eee6d6b
bbiao <41536+bbiao@users.noreply.github.com>
2024-06-02 14:00:37 +0800

    chore: remove trouble-v3 since it has been merged as defaults

--- lua/lazypde/plugins/lazy.lua
+++ lua/lazypde/plugins/lazy.lua
@@ -12,7 +12,6 @@ return {
   -- { import = "lazyvim.plugins.extras.editor.leap" },
   { import = "lazyvim.plugins.extras.editor.dial" },
   { import = "lazyvim.plugins.extras.editor.mini-files" },
-  { import = "lazyvim.plugins.extras.editor.trouble-v3" },
 
   { import = "lazyvim.plugins.extras.coding.mini-surround" },
   { import = "lazyvim.plugins.extras.coding.yanky" },
